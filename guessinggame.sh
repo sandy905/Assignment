38f40fa43d49d104a9cbabf4fb5f951bc09d26e0
sandy905 <45091258+sandy905@users.noreply.github.com>
2020-07-20 18:31:26 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,25 @@
+#!/usr/bin/env bash
+# File: guessinggame.sh
 
+count=$(ls -1 | wc -l)
+
+function correct_guess {    
+    if [[ $1 -lt $2 ]]        
+    then
+	echo "Too low!"
+    elif [[ $1 -gt $2 ]]
+    then
+	echo "Too high!"
+    else
+	echo ""
+	echo "Congratualations! You guessed right!"
+    fi
+}
+
+while [[ $count -ne $guess ]] 
+do
+    read -p "Guess how many files are in the current directory? " guess
+    # echo "You entered: $guess"
+    echo $(correct_guess $guess $count)
+    echo ""
+done
